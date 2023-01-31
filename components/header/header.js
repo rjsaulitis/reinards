3a425c71020e442d6ef785ef19782b2930c0a0de
Bryson Mercury <reinardssaulitis@gmail.com>
2023-01-31 23:41:46 +0200

    Fix header image not loading

--- components/header/header.js
+++ components/header/header.js
@@ -2,13 +2,15 @@ import css from "./header.module.scss"
 import Image from 'next/image'
 import 'animate.css';
 
+import LandingImg from "../../public/images/headers/home.jpg"
+
 export default function Header({children, cover, title, cta}) {
 
   if(cta) {
     return (
       <>
         <div className={css.background}>
-          <Image alt="Project Photo" src={"/../public/images/headers/"+cover} width="1920" height="1080" object-fit="cover" />
+          <Image alt="Project Photo" src={LandingImg} width="1920" height="1080" object-fit="cover" />
         </div>
         <div className={css.tint}>
             <h1 className="f1 mb4 tc animate__animated animate__slideInUp">{title}</h1>
@@ -22,7 +24,7 @@ export default function Header({children, cover, title, cta}) {
     return (
       <>
         <div className={css.background}>
-          <Image alt="Project Photo" src={"/../public/images/headers/"+cover} width="1920" height="1080" object-fit="cover"  />
+          <Image alt="Project Photo" src={"/images/headers/"+cover} width="1920" height="1080" object-fit="cover"  />
         </div>
         <div className={css.tint}>
             <h1 className="f0 ma0 animate__animated animate__fadeInUp animate__slow">{title}</h1>
