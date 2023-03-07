238f7ab166217308c9d605f417c883e0ccaedb1e
Bryson Mercury <reinardssaulitis@gmail.com>
2023-03-07 14:18:06 +0200

    Responsive fix

--- components/casestudies/half-size-photo.js
+++ components/casestudies/half-size-photo.js
@@ -8,12 +8,12 @@ export default function HalfSizePhoto({children, src, width, height, reverse}) {
     if(reverse) {
         return (
             <>
-                <div className={css.half_photo_wrap}>
+                <div className={css.half_photo_wrap_reverse}>
                     <div className="w-50-l">
                         <Image src={"/images/mockups/"+src} width={width} height={height}></Image>
                     </div>
                     <div className={"w-50-l tc flex flex-column justify-center items-center "+css.half_text}>
-                        <p className="w-40">
+                        <p className="w-80 w-40-l">
                             {children}
                         </p>
                         <Image src={"/images/mockups/"+src} width={width} height={height}></Image>
@@ -26,7 +26,7 @@ export default function HalfSizePhoto({children, src, width, height, reverse}) {
             <>
                 <div className={css.half_photo_wrap}>
                     <div className={"w-50-l tc flex flex-column justify-center items-center "+css.half_text}>
-                        <p className="w-40">
+                        <p className="w-80 w-40-l">
                             {children}
                         </p>
                         <Image src={"/images/mockups/"+src} width={width} height={height}></Image>
