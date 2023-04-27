3d803fd3c18d59be06b2d1d871d64bfa83cec64e
Bryson Mercury <reinardssaulitis@gmail.com>
2023-04-27 17:14:38 +0300

    replace main images to webp

--- components/casestudies/casestudies.js
+++ components/casestudies/casestudies.js
@@ -2,12 +2,12 @@ import css from "./casestudies.module.scss"
 import Image from 'next/image'
 import "animate.css"
 
-import Vdc from "../../public/images/mockups/vdc/vdc.jpg"
-import Lmg from "../../public/images/mockups/lmg/lmg.jpg"
-import Realdeal from "../../public/images/mockups/realdeal/a4-brochure.jpg"
-import Chalete from "../../public/images/mockups/chalete/chalete.jpg"
-import Alaise from "../../public/images/mockups/alaise/alaise.jpg"
-import Rosaella from "../../public/images/mockups/rosaella/desktop.jpg"
+import Vdc from "../../public/images/mockups/vdc/vdc.webp"
+import Lmg from "../../public/images/mockups/lmg/lmg.webp"
+import Realdeal from "../../public/images/mockups/realdeal/a4-brochure.webp"
+import Chalete from "../../public/images/mockups/chalete/chalete.webp"
+import Alaise from "../../public/images/mockups/alaise/alaise.webp"
+import Rosaella from "../../public/images/mockups/rosaella/desktop.webp"
 
 import Thumbnail from "./thumbnail";
 
