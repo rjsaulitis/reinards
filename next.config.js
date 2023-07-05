bda17f86437d4f0bd142a5cc594fc2ce05a9fa6c
Bryson Mercury <reinardssaulitis@gmail.com>
2023-07-05 17:34:43 +0300

    strategija redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/strategija',
+        destination: 'https://calendly.com/rjsaulitis/strategijas-sesija',
+        permanent: false
+      }
+    ];
+  }
 }
 
 module.exports = nextConfig
