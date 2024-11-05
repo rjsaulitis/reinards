5c4ee9f9a9e5d819d8fa296c64ae7a5d6eacda97
Bryson Mercury <reinardssaulitis@gmail.com>
2024-11-05 17:55:43 +0200

    Update header

--- pages/index.js
+++ pages/index.js
@@ -19,10 +19,12 @@ export default function Home() {
 
       <div id="landing" className='ph3 ph7-l pv6 pv7-l tc tl-l'>
           <div className='landing-details pv5 pv0-l'>
-            <h1 className='tl-l f1 f0 mv1 pv0'>Reinards</h1>
-            <p className='tl-l mv1 pb4'>Shopify Copywriting Specialist</p>
+            <h1 className='tl-l f1 f0 mv0 pv0'>Good Enough Doesn't <br/> Pay the Bills...</h1>
+            <p className='tl-l mv3 pt3 pb5'>Make your copy great and watch customers flood your 
+           your store, no matter what your sell!</p>
 
-            <CTA url="https://calendly.com/rjsaulitis/coffee">BOOK A MEETING</CTA>
+            <CTA url="https://calendly.com/rjsaulitis/coffee">Book a Consultation</CTA>
+            <img className='landing-proof mt3' src='/images/webp/proof.webp'/>
           </div>
           <img className='portrait' src='/images/webp/portrait.webp'/>
       </div>
@@ -30,12 +32,15 @@ export default function Home() {
 
       <div id="about" className='ph3 ph7-l pv3 pv5-l tc'>
         <h2 className='f2'>
-          Turn window-shoppers into loyal customers 🛍️
+          Who am I?
         </h2>
         <p>
-        I’m Reinards Jānis Saulītis, a copywriter with a background in digital marketing, graphic design, web development, branding and even arts. I’ve been dedicated to these fields since 2018, and since June 2023, I’ve focused exclusively on writing compelling copy for eCommerce stores. With experience as a web developer at Magebit and copywriting for Perfection Media, I understand the intricacies of ecommerce.
+        I’m Reinards Jānis Saulītis, a copywriter with a background in digital marketing, graphic design, web development, branding and even arts. I’ve been dedicated to these fields since 2018, and since June 2023, I’ve focused exclusively on mastering copy for eCommerce stores. With experience as a web developer at Magebit and copywriting for Perfection Media, I understand the intricacies of ecommerce.
         <br/><br/>
-        What sets me apart is my belief in connecting with people, not just pushing one's product to them. I strive to create authentic connections that foster brand loyalty and compounding long-term gains. I’m a deeply creative person and always push myself to deliver better, more resonant copy.
+        I’ve immersed myself in books by top experts, worked with a wide range of clients, and refined my skills through real-world projects. The time with two e-commerce development agencies has given me invaluable, firsthand insights into what drives online sales and customer engagement.
+        <br/><br/>
+        Add to that countless hours spent learning from industry leaders through videos, articles, and online resources—I’ve dedicated myself to mastering the nuances of copy and marketing so I can deliver the best results for you
+
         </p>
       </div>
 
@@ -43,6 +48,30 @@ export default function Home() {
       <a href="http://perfectionmedia.lv" target="_blank" title="Perfection Media"><img className='review dn-ns' src='/images/webp/perfection-mobile-en.webp'/></a>
 
       <img className='portfolio dn db-ns' src='/images/webp/work.webp'/>
+{/* 
+
+          <div className='services ph7 pv6'>
+            <div className='flex'>
+              <img className='review dn db-ns' src='/images/consult.png'/>
+              <div className='ml4'>
+                <h2 className='f2'>Consulting</h2>
+                <p>
+                I will help you increase conversion rates, average order value and in general make your copy more persuasive by understanding your audience and fixing ...
+                </p>
+              </div>
+            </div>
+            <div className='flex mt5'>
+              <div className='ml4'>
+                <h2 className='f2'>Copywriting</h2>
+                <p>
+                I will help you increase conversion rates, average order value and in general make your copy more persuasive by understanding your audience and fixing ...
+                </p>
+              </div>
+              <img className='review dn db-ns' src='/images/write.png'/>
+            </div>
+          </div> */}
+
+
       <img className='portfolio dn-ns' src='/images/webp/work-mobile.webp'/>
 
 
